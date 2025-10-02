340c10e9b8270a47c0d9ae13207cb6ced6368332
Rupesh Sharma <102rupeshsharma@gmail.com>
2025-10-02 14:09:14 +0530

    Initial project commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <h1>Happy Birthday 🎉</h1>
+        <p>With all my love, here's something special just for you 💝🎁</p>
+
+        <div className="gift-card">
+          <img src="/gift-card-image.png" alt="Gift Card" />
+        </div>
+
+      </div>
+    </div>
+  );
+}
